ae77468bb667abd2ae9934dbd11113b0b19fb66c
Alvaro Garcia <alvarogarciaf@users.noreply.github.com>
2026-08-10 10:47:06 -0300

    Fix markdown extra newline bugs and cursor boundaries

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tasker-agf-v1786368372168";
+const CACHE_NAME = "tasker-agf-v1786369576660";
 const PRECACHE_URLS = ["/", "/manifest.json", "/logo.svg"];
 
 // Detect localhost to bypass caching for development HMR
