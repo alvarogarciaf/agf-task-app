c8e240ea0c036ad8e25931473c99bb70acd54138
Alvaro Garcia <alvarogarciaf@users.noreply.github.com>
2026-08-11 11:20:42 -0300

    chore: bump service worker cache version

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tasker-agf-v1786446907793";
+const CACHE_NAME = "tasker-agf-v1786456644780";
 const PRECACHE_URLS = ["/", "/manifest.json", "/logo.svg"];
 
 // Detect localhost to bypass caching for development HMR
