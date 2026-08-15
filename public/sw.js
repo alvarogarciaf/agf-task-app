3080f6e20c3f1dc616e96634b6eb73a19e4d78c8
Alvaro Garcia <alvarogarciaf@users.noreply.github.com>
2026-08-14 22:07:59 -0300

    fix: remove auto-focus on navigation to inbox

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tasker-agf-v1786755899056";
+const CACHE_NAME = "tasker-agf-v1786756049297";
 const PRECACHE_URLS = ["/", "/manifest.json", "/logo.svg"];
 
 // Detect localhost to bypass caching for development HMR
