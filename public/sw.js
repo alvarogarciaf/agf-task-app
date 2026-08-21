c6937a77e092640bfc4bfa15fc64d63107f0da60
Alvaro Garcia <alvarogarciaf@users.noreply.github.com>
2026-08-21 08:26:40 -0300

    chore(release): bump service worker cache version to tasker-agf-v1787311572794

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tasker-agf-v1787276825434";
+const CACHE_NAME = "tasker-agf-v1787311572794";
 const IMAGE_CACHE_NAME = "tasker-images-v1";
 const PRECACHE_URLS = ["/", "/manifest.json", "/logo.svg", "/placeholder.jpg"];
 
