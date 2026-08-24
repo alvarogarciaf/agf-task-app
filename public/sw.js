21d94fc1bd2b5e1c6a7297e543e17986a198068f
Alvaro Garcia <alvarogarciaf@users.noreply.github.com>
2026-08-24 07:58:08 -0300

    chore: remove additional specified icons from constants registry

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tasker-agf-v1787531678673";
+const CACHE_NAME = "tasker-agf-v1787569053731";
 const IMAGE_CACHE_NAME = "tasker-images-v1";
 const PRECACHE_URLS = ["/", "/manifest.json", "/logo.svg", "/placeholder.jpg"];
 
