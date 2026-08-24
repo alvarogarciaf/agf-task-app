bf1a66a6bb02d0bfbe261a3acb8138a894a141cb
Alvaro Garcia <alvarogarciaf@users.noreply.github.com>
2026-08-23 21:28:28 -0300

    chore: clean up and curate available icons list based on user selections

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tasker-agf-v1787530925963";
+const CACHE_NAME = "tasker-agf-v1787531276711";
 const IMAGE_CACHE_NAME = "tasker-images-v1";
 const PRECACHE_URLS = ["/", "/manifest.json", "/logo.svg", "/placeholder.jpg"];
 
