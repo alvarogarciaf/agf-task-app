12ab5005152ca36198858aa6ff0ebeb2d7509f7e
Alvaro Garcia <alvarogarciaf@users.noreply.github.com>
2026-09-14 22:42:54 -0300

    fix(deps): downgrade firebase-admin to v13 to satisfy firebase-frameworks peer dependency

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tasker-agf-v1789435958130";
+const CACHE_NAME = "tasker-agf-v1789436529686";
 const IMAGE_CACHE_NAME = "tasker-images-v1";
 const PRECACHE_URLS = ["/", "/manifest.json", "/logo.svg", "/placeholder.jpg"];
 
